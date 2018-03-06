0d2a267e957fb582629ad016c15f2cabaf4732a3
takahiro.kameda <ttt.kameda.10@gmail.com>
2018-03-06 23:55:04 +0000

    add status column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302123500) do
+ActiveRecord::Schema.define(version: 20180306233520) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
